fb557870aab5d39a03be86f5a717c2afab3e8cd0
claire-se-practice <129072310+claire-se-practice@users.noreply.github.com>
2023-06-06 21:42:59 +0900

    Update SeleniumTest1Test.java

--- src/test/java/SeleniumTest1Test.java
+++ src/test/java/SeleniumTest1Test.java
@@ -77,7 +77,7 @@ public class SeleniumTest1Test {
         driver.findElement(By.id("minidaum")).click();
         Thread.sleep(1000);
         driver.findElement(By.linkText("Daum")).click();
-        Thread.sleep(1000);
+        Thread.sleep(100);
         driver.close();
     }
 }
